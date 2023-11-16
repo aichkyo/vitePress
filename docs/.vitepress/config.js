4af54a87615a008f4865e6bd384cf669a6e1c9f8
AichKyo <754555298@qq.com>
2023-11-17 03:03:34 +0800

    refactor: 修改目录结构

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'AickKyo Docs',
+  description: 'Just playing around.',
+  lang: 'zh-CN',
+
+  // 主题配置
+  themeConfig: {
+    siteTitle: '前端',
+    siteTitle: false,
+    logo: '/logo.png',
+
+    head: [
+      ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+
+    search: {
+      provider: 'local'
+    },
+
+    nav: [
+      // { text: 'Home', link: '/' },
+      { text: 'hr公共组件', link: '/hr-components/', activeMatch: '/hr-components/' },
+      { text: '技术分享', link: '/share/', activeMatch: '/share/' },
+      // { 
+      //   text: '技术分享',
+      //   items: [
+      //     { text: 'npm包', link: '/share/npmPackage' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { 
+      //       // Title for the section.
+      //       text: '下拉分组小标题',
+      //       items: [
+      //         { text: 'Section A Item A', link: '...' },
+      //         { text: 'Section B Item B', link: '...' }
+      //       ]
+      //     }
+      //   ]
+      // },
+      { text: 'Changelog', link: 'https://github.com/...' }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/aichkyo/aichkyo.github.io' },
+      // You can also add custom icons by passing SVG as string:
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        },
+        link: '...',
+        // You can include a custom label for accessibility too (optional but recommended):
+        ariaLabel: 'cool link'
+      }
+    ],
+
+    sidebar: {
+      "share": [
+        {
+          text: '开发vue公共组件库',
+          collapsed: false,
+          items: [
+            { text: '开发组件库', link: '/share/hroa-ui/开发组件库' },
+            { text: '认识nrm', link: '/share/hroa-ui/认识nrm' },
+            { text: '发布npm包', link: '/share/hroa-ui/发布npm包' },
+            { text: '使用组件库', link: '/share/hroa-ui/使用组件库' },
+          ]
+        },
+        {
+          text: '技术分享侧边栏2',
+          collapsed: true,
+          items: [
+            { text: 'Item C', link: '/item-c' },
+            { text: 'Item D', link: '/item-d' },
+          ]
+        }
+      ],
+      "hr-components": [
+        {
+          text: '公共组件1',
+          collapsed: false,
+          items: [
+            { text: '组件1', link: '/item-a' },
+            { text: '组件2', link: '/item-b' },
+          ]
+        },
+        {
+          text: '公共组件2',
+          collapsed: true,
+          items: [
+            { text: 'Item C', link: '/item-c' },
+            { text: 'Item D', link: '/item-d' },
+          ]
+        }
+      ]
+    }
+
+
+
+    // footer: {
+    //   message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+    //   copyright: 'Copyright © 2019-present <a href="https://github.com/yyx990803">Evan You</a>'
+    // }
+  },
+
+  
+}
